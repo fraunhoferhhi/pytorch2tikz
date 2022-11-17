fix scaling

pycore/arch.py
@@ -14,7 +14,7 @@ class Architecure:
                  image_path='./input_{i}.png',
                  ignore_layers=['batchnorm']) -> None:
         self._block_factory = BlockFactory(start_size, block_offset, scale_factor)
-        self._blocks = BlockSequence(self._block_factory, ignore_layers)
+        self._block_sequence = BlockSequence(self._block_factory, ignore_layers)
 
         self.image_path = image_path
 
@@ -44,7 +44,7 @@ class Architecure:
 
         # set inputs
         if not last_in_equals_out:
-            self._blocks.append_input(input, module)
+            self._block_sequence.append_input(input, module)
 
         # check if tensor shape is equal to previous tensor shape, if not start new grouped blocks
         try:
@@ -54,7 +54,7 @@ class Architecure:
 
         # if not same_depth add gap
         if not same_depth and 'pooling' not in str(type(module)):
-            self._blocks.add_gap()
+            self._block_sequence.add_gap()
             if len(out_shape) == len(in_shape) and last_in_equals_out:
                 scale = np.array(out_shape) / np.array(in_shape)
 
@@ -63,15 +63,15 @@ class Architecure:
                 elif len(scale) < 3:
                     return
 
-                self._blocks.scale(scale)
+                self._block_sequence.scale(scale)
 
         # add current module to blocks
-        self._blocks.append(module)
+        self._block_sequence.append(module)
 
         self._tensor_size = None
 
     def finalize(self) -> str:
         out = ''
-        for b in self._blocks:
+        for b in self._block_sequence:
             out += f'\n{b}'
         return out

pycore/block_factory.py
@@ -22,7 +22,7 @@ class BlockFactory:
         self.last_block_id = 0
     
     def create(self, block_class, i: int) -> Block:
-        new_block = block_class(
+        new_block: Block = block_class(
                  i,
                  size = self.size,
                  offset = self.current_offset,
@@ -38,24 +38,26 @@ class BlockFactory:
         else:
             to = self.to
     
-        self.current_offset += np.array((0.0, self.size[1] + self.offset, 0.0))
+        self.current_offset += np.array((-self.current_offset[0], self.size[1] + self.offset, 0.0))
+
         offset = tuple(self.current_offset)
         self.current_offset = np.zeros(3)
 
         if x.ndim > 3:
             im_path = self.image_path.replace('{i}', str(self.last_block_id + 1))
             save_image(x[0], im_path)
-            return ImgInputBlock(self.last_block_id + 1,
-                         im_path,
-                         to=to,
-                         offset=offset,
-                         size=self.size
-                        )
+
+            new_block = ImgInputBlock(self.last_block_id + 1,
+                                      im_path,
+                                      to=to,
+                                      offset=offset,
+                                      size=self.size)
         else:
-            return VecInputBlock(self.last_block_id + 1,
-                                 to=to,
-                                 offset=offset,
-                                 size=self.size)
+            new_block = VecInputBlock(self.last_block_id + 1,
+                                      to=to,
+                                      offset=offset,
+                                      size=self.size)
+        return new_block
     
     def add_gap(self, axis=0):
         offset = np.zeros(3)